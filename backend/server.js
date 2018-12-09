4d4579a88cc04fdd83ec63661661001eb2af4917
Victor <victor.schirmann@gmail.com>
2018-12-09 18:48:28 +0100

    Backend accept or reject order done

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
 var db;
 
@@ -25,8 +26,54 @@ app.post('/orders', (req, res) => {
         if (err) console.log(err)
         console.log('saved to database')
         res.send({ status: err ? 'error' : 'success' });
-      });
+    });
 });
+
+app.get('/orders', (req, res) => {
+    db.collection('orders').find().toArray((err, results) => {
+        if (err) return console.log(err);
+        res.send(results);
+    });
+});
+
+app.post('/orders/accept', (req, res) => {
+    console.log(req.id)
+    db.collection('orders').findOne({ _id: ObjectID(req.body._id) })
+    .then(result => {
+        db.collection('orders').updateOne({ _id: result._id }, { $set: { ...result, status: "accepted" } })
+            .then(obj => {
+                res.send({ status: "success", order: obj });
+            })
+            .catch(error => {
+                console.log(error);
+                res.send({ status: "error", error: error });
+            });
+    })
+    .catch(error => {
+        console.log(error);
+        res.send({ status: "error", error: error });
+    })
+});
+
+app.post('/orders/reject', (req, res) => {
+    console.log(req.id)
+    db.collection('orders').findOne({ _id: ObjectID(req.body._id) })
+    .then(result => {
+        db.collection('orders').updateOne({ _id: result._id }, { $set: { ...result, status: "rejected" } })
+            .then(obj => {
+                res.send({ status: "success", order: obj });
+            })
+            .catch(error => {
+                console.log(error);
+                res.send({ status: "error", error: error });
+            });
+    })
+    .catch(error => {
+        console.log(error);
+        res.send({ status: "error", error: error });
+    })
+});
+
 
 app.listen(3003, () => {
     console.log('MerciGustave API Server is running on port 3003');
